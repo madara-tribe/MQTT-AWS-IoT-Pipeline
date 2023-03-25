079d983a606e6aa5368d6e71e870630ad221d56d
madara-tribe <48679574+madara-tribe@users.noreply.github.com>
2023-03-25 22:46:27 +0900

    Update testMQTT.py

--- aws-iot-device-sdk-python/testMQTT.py
+++ aws-iot-device-sdk-python/testMQTT.py
@@ -4,6 +4,9 @@ import time
 import json
 
 ROOTDIR='../keys'
+THING_NAME = 'testMQTT'
+TOPIC_NAME = 'device/22/data'
+
 class IotMqttClient:
 
     ROOT_CA_PATH = os.path.join(ROOTDIR, 'AmazonRootCA1.pem')
@@ -11,19 +14,18 @@ class IotMqttClient:
     PRIVATE_KEY_PATH = os.path.join(ROOTDIR, 'private.pem.key')
     IOT_ENDPOINT = 'a3phz77nfdgsr1-ats.iot.ap-northeast-1.amazonaws.com'
     IOT_PORT = 8883
-    IOT_CLIENT_ID = 'testMQTT'
     __my_iot_mqtt_client = None
     __is_connected = False
 
     def __init__(self):
         from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 
-        # クライアントの設定
-        self.__my_iot_mqtt_client = AWSIoTMQTTClient(self.IOT_CLIENT_ID)
+        # setup client
+        self.__my_iot_mqtt_client = AWSIoTMQTTClient(THING_NAME)
         self.__my_iot_mqtt_client.configureEndpoint(self.IOT_ENDPOINT, self.IOT_PORT)
         self.__my_iot_mqtt_client.configureCredentials(self.ROOT_CA_PATH, self.PRIVATE_KEY_PATH, self.CERTIFICAT_PATH)
 
-        # 接続情報の設定
+        # setup access infomation
         self.__my_iot_mqtt_client.configureAutoReconnectBackoffTime(1, 32, 20)
         self.__my_iot_mqtt_client.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
         self.__my_iot_mqtt_client.configureDrainingFrequency(2)  # Draining: 2 Hz
@@ -36,7 +38,7 @@ class IotMqttClient:
         self.__my_iot_mqtt_client.onOffline = on_offline
         self.__my_iot_mqtt_client.onOnline = on_online
 
-        # 接続開始
+        # start access
         self.__my_iot_mqtt_client.connect()
 
     def publish(self, IOT_TOPIC, unpacked_data):
@@ -55,7 +57,7 @@ def main():
        time.sleep(1)
        send_data = {'key':count}
        json_data = json.dumps(send_data)
-       iot_mqtt_client.publish('testMQTT/mqtt_client', json_data)
+       iot_mqtt_client.publish(os.path.join(THING_NAME, TOPIC_NAME), json_data)
 
 if __name__ == '__main__':
     main()
